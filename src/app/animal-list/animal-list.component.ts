18a8c193d3b98c1e4fda3265918d622f69acf639
GojkoMaric <gojko.maric@yahoo.com>
2018-01-09 19:55:35 +0100

    1 exam

---
+++ src/app/animal-list/animal-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrls: ['./animal-list.component.css']
+})
+export class AnimalListComponent implements OnInit {
+  animals: Array<Object>;
+
+  constructor() {
+    this.animals=
+    [
+      {
+        type: 'tiger',
+        name: 'Tigar',
+        date: '01-01-01' 
+      },
+      {
+        type: 'lion',
+        name: 'Lav',
+        date: '02-03-02'
+      },
+      {
+        type: 'monkey',
+        name: 'Marmun',
+        date: '05-07-02'
+      },
+      {
+        type: 'fox',
+        name: 'Lisica',
+        date: '09-08-06'
+      },
+      {
+        type: 'bear',
+        name: 'Medved',
+        date: '05-07-09'
+      }
+
+    ];
+   }
+
+  ngOnInit() {
+  }
+
+}
